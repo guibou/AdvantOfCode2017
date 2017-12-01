3cbe15b330db8878c02133bca7a4908a241f72fe
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2017-12-01 10:57:41 +0100

    [Template] set 0 for the first examples to get a working default file

--- src/DayX.hs
+++ src/DayX.hs
@@ -18,9 +18,9 @@ day' code = undefined
 test = hspec $ do
   describe "simple examples" $ do
     it "of first star" $ do
-      day "" `shouldBe` undefined
+      day "" `shouldBe` 0
     it "of second star" $ do
-      day' "" `shouldBe` undefined
+      day' "" `shouldBe` 0
 --  describe "woks" $ do
 --    it "on first star" $ do
 --      day fileContent `shouldBe` 1228

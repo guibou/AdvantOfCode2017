4035d9f569c2fed4962595bb90b900f175813317
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2017-12-07 15:49:23 +0100

    Add an utility function to load all tests

---
+++ src/All.hs
@@ -0,0 +1,18 @@
+module All where
+
+import Day1
+import Day2
+import Day3
+import Day4
+import Day5
+import Day6
+import Day7
+
+test = do
+  Day1.test
+  Day2.test
+  Day3.test
+  Day4.test
+  Day5.test
+  Day6.test
+  Day7.test
